118bb2f387e227a221921c8cc548796cea9eba67
dayeong <daye9712@naver.com>
2020-07-08 14:56:55 +0900

    Create Employee.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Employee from './components/Employee';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const employees = [
+  {
+    'id' : 1,
+    'image' : 'http://placeimg.com/64/64/1',
+    'name' : '홍길동',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'http://placeimg.com/64/64/2',
+    'name' : '홍길순',
+    'birthday' : '970831',
+    'gender' : '여자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 3,
+    'image' : 'http://placeimg.com/64/64/2',
+    'name' : '김길동',
+    'birthday' : '981207',
+    'gender' : '남자',
+    'job' : '프로그래머'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          employees.map(c => {
+            return (
+              <Employee
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}></Employee>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
